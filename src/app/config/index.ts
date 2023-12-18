1ce293c4de9787e7eec5f8da3df59025cb0a2e4c
nikettongare <nikettongaren24@gmail.com>
2023-12-18 21:23:44 +0530

    backend domain added in config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,2 +1,2 @@
-export const BACKEND_ENDPOINT = "http://localhost:8080";
+export const BACKEND_ENDPOINT = "https://goodshelf.nikettongare.com/";
 export const PRODUCTS_ENDPOINT = `${BACKEND_ENDPOINT}/products`;
